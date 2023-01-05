782ce6d8cd024b1619fb0402bd9313770fcefdaa
loc.nguyen <loc.nguyen@napaglobal.com>
2023-01-05 16:17:22 +0700

    set up stores + get address in trezor

--- src/components/templates/trezor/Trezor.tsx
+++ src/components/templates/trezor/Trezor.tsx
@@ -1,20 +1,11 @@
 import {
-  TableContainer,
-  Table,
-  Thead,
-  Tr,
-  Th,
-  Tbody,
-  Td,
-  Tfoot,
-  Heading,
-  Box,
-  useColorModeValue,
+  Box, Heading, Table, TableContainer, Tbody,
+  Td, Th, Thead,
+  Tr, useColorModeValue
 } from '@chakra-ui/react';
 import { useEvmWalletNFTTransfers } from '@moralisweb3/next';
+import { useTrezorAccount } from 'hooks/useTrezor';
 import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
-import { getEllipsisTxt } from 'utils/format';
 import { useNetwork } from 'wagmi';
 
 const TrezorAccount = () => {
@@ -26,28 +17,35 @@ const TrezorAccount = () => {
     chain: chain?.id,
   });
 
-  useEffect(() => console.log('transfers: ', transfers), [transfers]);
+  const { trezorAccounts, getFullAccounts } = useTrezorAccount();
+
+  // useEffect(() => console.log('transfers: ', transfers, data), [transfers]);
+
+  console.log(trezorAccounts);
 
   return (
     <>
       <Heading size="lg" marginBottom={6}>
         Trezor Account
       </Heading>
-      {transfers?.length ? (
+      {trezorAccounts?.length ? (
         <Box border="2px" borderColor={hoverTrColor} borderRadius="xl" padding="24px 18px">
           <TableContainer w={'full'}>
             <Table>
               <Thead>
                 <Tr>
-                  <Th>Token</Th>
                   <Th>Token Id</Th>
-                  <Th>From</Th>
-                  <Th>To</Th>
-                  <Th>Type</Th>
-                  <Th>Date</Th>
-                  <Th isNumeric>Tx Hash</Th>
+                  <Th>Address</Th>
                 </Tr>
               </Thead>
+              <Tbody>
+                {trezorAccounts?.map((account: any, key: number) => (
+                  <Tr key={key} _hover={{ bgColor: hoverTrColor }} cursor="pointer">
+                    <Td>{key + 1}</Td>
+                    <Td>{account.address}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
             </Table>
           </TableContainer>
         </Box>
